de662011339c942d4b5875fbf02d6ab8a89e7ff2
vmarc <vmarc@DESKTOP-BNC8SKO>
2024-07-11 18:40:02 -0600

    Agregar archivos de proyecto.

---
+++ APPTodo4955511/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using APPTodo4955511.Views;
+namespace APPTodo4955511
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(TodoItemPage), typeof(TodoItemPage));
+        }
+    }
+}
---
+++ APPTodo4955511/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace APPTodo4955511
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
